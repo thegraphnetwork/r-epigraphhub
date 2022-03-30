0ec8a6f59f17723dc17e5076f016e82fe421a156
Daniel C. P. Câmara <dcpcamara@gmail.com>
2022-03-29 21:02:34 -0300

    added unit test for df_epinow
    added unit test for df_epinow function

---
+++ tests/testthat/test-df_epinow.R
@@ -0,0 +1,24 @@
+testthat::test_that("errors", {
+
+    df <- data.frame(date = rep(seq(as.Date("2022-01-01"), by = "day", length.out = 60), 2),
+                   cases = rep(sample.int(n = 100, size = 60, replace = TRUE), 2),
+                   region = c(rep("Region A", 60), rep("Region B", 60)))
+
+  testthat::expect_error(
+    df_epinow(cases = cases, date = date, regional = FALSE),
+    "Dataset not provided."
+  )
+  testthat::expect_error(
+    df_epinow(x = df, date = date, regional = FALSE),
+    "Vector of cases not provided."
+  )
+  testthat::expect_error(
+    df_epinow(x = df, cases = casesx, regional = FALSE),
+    "Vector of dates not provided."
+  )
+  testthat::expect_error(
+    df_epinow(x = df, cases = cases, date = date, regional = TRUE),
+    "Vector of regions not provided."
+  )
+
+})
