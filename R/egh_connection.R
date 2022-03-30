5ae82fa2ffd8a3e54e0dd7669e32265cac8b7315
Daniel C. P. Câmara <dcpcamara@gmail.com>
2022-03-30 07:15:21 -0300

    Update egh_connection.R
    Minor change made

--- R/egh_connection.R
+++ R/egh_connection.R
@@ -15,8 +15,7 @@
 #' Watch out for hardcoded login and password.
 #'
 #' @examples \dontrun{
-#' egh_connection(auto_connect = TRUE, use_env = TRUE, path_env = "../.env")
-#'
+#' egh_connection(auto_connect = TRUE, use_env = TRUE, path_env = "./.env")
 #' egh_connection(auto_connect = TRUE, use_env = FALSE, user = "user", password = "password")
 #' }
 #' @import dotenv
